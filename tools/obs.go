899213778512634eb18d7f68833bee25eafa7a3b
JasonLeemz <limingze610@gmail.com>
2023-01-12 14:44:32 +0800

    fix filename extra err

--- tools/obs.go
+++ tools/obs.go
@@ -131,7 +131,7 @@ func pushStream(filePath, movieName, rtmpUrl string) error {
 			"-c:v", "libx264",
 			"-c:a", "copy",
 			"-b:a", "192k",
-			"-vf", "\"drawtext=text=" + movieName + ":x=10:y=10:fontsize=10:fontcolor=white:shadowy=2\"",
+			"-vf", "\"drawtext=fontfile=rl.ttf: text" + movieName + ":x=10:y=10:fontsize=10:fontcolor=white:shadowy=2\"",
 			"-strict", "-2",
 			"-f", "flv", rtmpUrl,
 		}
